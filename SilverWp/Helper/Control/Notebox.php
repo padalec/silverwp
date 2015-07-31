e0a1e70cd81c89ac67c3d94db50b76575104487e
padalec <michal@silversite.pl>
2015-07-31 08:39:16 +0200

    add default name for Notebox

--- SilverWp/Helper/Control/Notebox.php
+++ SilverWp/Helper/Control/Notebox.php
@@ -16,44 +16,48 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-/*
- Repository path: $HeadURL: $
- Last committed: $Revision: $
- Last changed by: $Author: $
- Last changed date: $Date: $
- ID: $Id: $
-*/
 namespace SilverWp\Helper\Control;
 
 if ( ! class_exists( '\SilverWp\Helper\Control\Notebox' ) ) {
 
-    /**
-     *
-     * Notebox isn't really a control, it's only serve as a
-     * way to give a notice to your users in a note box fashion.
-     *
-     * @category WordPress
-     * @package SilverWp
-     * @subpackage Helper\Control
-     * @author Michal Kalkowski <michal at silversite.pl>
-     * @copyright Dynamite-Studio.pl & silversite.pl 2015
-     * @version $Revision:$
-     */
-    class Notebox extends ControlAbstract {
-        protected $type = 'notebox';
+	/**
+	 *
+	 * Notebox isn't really a control, it's only serve as a
+	 * way to give a notice to your users in a note box fashion.
+	 *
+	 * @category   WordPress
+	 * @package    SilverWp
+	 * @subpackage Helper\Control
+	 * @author     Michal Kalkowski <michal at silversite.pl>
+	 * @copyright  Dynamite-Studio.pl & silversite.pl 2015
+	 * @version    $Revision:$
+	 */
+	class Notebox extends ControlAbstract {
+		protected $type = 'notebox';
 
-        /**
-         * Status of the note, supported status are:
-         * normal, info, warning, error, success, default to normal.
-         *
-         * @param string $status
-         *
-         * @access public
-         */
-        public function setStatus( $status ) {
-            $this->setting[ 'status' ] = $status;
-
-            return $this;
+		/**
+		 * Class constructor
+		 *
+		 * @param string $name default empty string
+		 *
+		 * @throws Exception
+		 */
+		public function __construct( $name = '' ) {
+			parent::__construct( $name );
         }
-    }
+
+		/**
+		 * Status of the note, supported status are:
+		 * normal, info, warning, error, success, default to normal.
+		 *
+		 * @param string $status
+		 *
+		 * @access public
+		 */
+		public function setStatus( $status ) {
+			$this->setting['status'] = $status;
+
+			return $this;
+		}
+	}
 }
